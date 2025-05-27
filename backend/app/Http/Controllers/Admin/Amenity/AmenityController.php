b19e38103bf0b5b64ec6b591a1f2bf15a183999c
logn <logn021004@gmail.com>
2025-05-27 12:50:21 +0700

    CMS Admin: Thêm chức năng quản lý tiện ích

---
+++ backend/app/Http/Controllers/Admin/Amenity/AmenityController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Amenity;
+
+use App\DataTables\Admin\Amenity\AmenityDataTable;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Amenity\AmenityRequest;
+use App\Models\Amenity;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AmenityController extends Controller
+{
+
+    private $data;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getView(): array
+    {
+        return [
+            'index' => 'admin.amenity.index',
+            'create' => 'admin.amenity.create',
+            'edit' => 'admin.amenity.edit'
+        ];
+    }
+
+    public function getRoute(): array
+    {
+        return [
+            'index' => 'admin.amenity.index',
+            'create' => 'admin.amenity.create',
+            'edit' => 'admin.amenity.edit',
+            'delete' => 'admin.amenity.delete'
+        ];
+    }
+
+    public function index(AmenityDataTable $dataTable)
+    {
+        return $dataTable->render($this->view['index'], [
+            'breadcrumbs' => $this->crums->add(__('Danh sách Tiện ích'))
+        ]);
+    }
+
+    public function create()
+    {
+        return view($this->view['create'], [
+            'breadcrumbs' => $this->crums->add(
+                __('Danh sách tiện ích'),
+                route($this->route['index'])
+            )->add('Thêm tiện ích')
+        ]);
+    }
+
+    public function store(AmenityRequest $request)
+    {
+        $this->data = $request->validated();
+        if (Amenity::where('name', '=', $this->data['name'])->exists()) {
+            return redirect()->route($this->route['create'])->with('error', 'Tên tiện ích đã tồn tại');
+        }
+        try {
+            DB::beginTransaction();
+            Amenity::create($this->data);
+            DB::commit();
+            return redirect()->route($this->route['index'])->with('success', 'Thêm thành công');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return redirect()->route($this->route['create'])->with('error', 'Thêm thất bại');
+        }
+    }
+
+    public function edit($id)
+    {
+        $amenity = Amenity::where('id', $id)
+            ->whereHas('parent')
+            ->first();
+        if(empty($amenity))
+            $amenity = Amenity::find($id);
+        return view($this->view['edit'], [
+            'breadcrumbs' => $this->crums->add(('Danh sách tiện ích'), route($this->route['index']))->add('Sửa tiện ích'),
+            'amenity' => $amenity,
+        ]);
+    }
+
+    public function update(AmenityRequest $request)
+    {
+        $this->data = $request->validated();
+        if (Amenity::where('name', '=', $this->data['name'])->exists()) {
+            return redirect()->route($this->route['edit'],$this->data['id'])->with('error', 'Tên tiện ích đã tồn tại');
+        }
+        try {
+            DB::beginTransaction();
+            $amenity = Amenity::findOrFail($this->data['id']);
+            unset($this->data['id']);
+            $amenity->update($this->data);
+            DB::commit();
+            return redirect()->route($this->route['index'])->with('success', 'Thêm thành công');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return redirect()->route($this->route['create'])->with('error', 'Thêm thất bại');
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            DB::beginTransaction();
+            $this->data = Amenity::find($id);
+            $this->data->delete();
+            DB::commit();
+            return redirect()->route($this->route['index'])->with('success', 'Xóa thành công');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return redirect()->route($this->route['index'])->with('error', 'Xóa thất bại');
+        }
+    }
+}
--- backend/config/admin_sidebar.php
+++ backend/config/admin_sidebar.php
@@ -3,23 +3,23 @@
 return [
     [
         'title' => 'Dashboard',
-        'routeName' => 'admin',
+        'routeName' => 'admin.dashboard',
         'icon' => '<i class="ti ti-home"></i>',
         'sub' => []
     ],
     [
-        'title' => 'Quản lý Lớp Học',
+        'title' => 'Quản lý Tiện ích',
         'routeName' => null,
-        'icon' => '<i class="ti ti-school"></i>',
+        'icon' => '<i class="ti ti-tournament"></i>',
         'sub' => [
             [
-                'title' => 'Thêm Lớp',
-                'routeName' => 'admin',
+                'title' => 'Thêm Tiện ích',
+                'routeName' => 'admin.amenity.create',
                 'icon' => '<i class="ti ti-plus"></i>',
             ],
             [
-                'title' => 'DS Lớp học',
-                'routeName' => null,
+                'title' => 'DS Tiện ích',
+                'routeName' => 'admin.amenity.index',
                 'icon' => '<i class="ti ti-list"></i>',
             ]
         ]
